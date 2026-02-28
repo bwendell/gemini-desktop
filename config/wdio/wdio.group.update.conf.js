fb1ffcbaf992027e537633e1e609f301dfdec830
Benjamin Wendell <github@benwendell.com>
2026-02-28 15:56:30 +0800

    test(e2e): consolidate auto-update specs (#146)
    * test(e2e): consolidate auto-update specs
    
    * test(e2e): stabilize auto-update spec bridge usage

--- config/wdio/wdio.group.update.conf.js
+++ config/wdio/wdio.group.update.conf.js
@@ -3,12 +3,6 @@ import { baseConfig } from './wdio.base.conf.js';
 export const config = {
     ...baseConfig,
     specs: [
-        '../../tests/e2e/auto-update-error-recovery.spec.ts',
-        '../../tests/e2e/auto-update-happy-path.spec.ts',
-        '../../tests/e2e/auto-update-interactions.spec.ts',
-        '../../tests/e2e/auto-update-persistence.spec.ts',
-        '../../tests/e2e/auto-update-platform.spec.ts',
-        '../../tests/e2e/auto-update-toggle.spec.ts',
-        '../../tests/e2e/auto-update-tray.spec.ts',
+        '../../tests/e2e/auto-update.spec.ts',
     ],
 };
